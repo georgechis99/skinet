4c2a32fe53d9fe661b9f986e8d23889c837d18a0
George Chis <george.chis2014@gmail.com>
2022-08-17 13:10:09 +0300

    End of section 6

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -9,12 +9,24 @@ namespace Core.Specifications
     public class BaseSpecification<T> : ISpecification<T>
     {
 
+        //Properties for QUERYING
          public Expression<Func<T, bool>> Criteria {get;}
 
         public List<Expression<Func<T, object>>> Includes {get;} = 
             new List<Expression<Func<T, object>>>();
 
+        public Expression<Func<T, object>> OrderBy {get; private set;}
 
+        public Expression<Func<T, object>> OrderByDescending {get; private set;}
+
+        public int Take {get; private set;}
+
+        public int Skip {get; private set;}
+
+        public bool IsPagingEnabled {get; private set;}
+
+
+        //CONSTRUCTORS
         public BaseSpecification(Expression<Func<T, bool>> criteria)
         {
             Criteria = criteria;
@@ -25,8 +37,23 @@ namespace Core.Specifications
         }
 
         
+        //PRIVATE methods for adding query functioanlities
         protected void AddInclude(Expression<Func<T, object>> includeExpression){
             Includes.Add(includeExpression);
+        }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression){
+            OrderBy = orderByExpression;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression){
+            OrderByDescending = orderByDescExpression;
+        }
+
+        protected void ApplyPaging(int skip, int take){
+            Skip = skip;
+            Take = take;
+            IsPagingEnabled = true;
         }
     }
 }
