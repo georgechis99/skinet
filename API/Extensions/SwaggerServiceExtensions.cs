15eb46deb44bc36d4d4acac75cb9319b1f9028cd
George Chis <george.chis2014@gmail.com>
2022-08-31 12:13:32 +0300

    End of section 16

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -8,17 +8,44 @@ namespace API.Extensions
 {
     public static class SwaggerServiceExtensions
     {
-        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services){
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
 
-             services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            
+            services.AddSwaggerGen(c =>
+           {
+               c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+
+               var securitySchema = new OpenApiSecurityScheme
+               {
+                   Description = "JWT Auth Bearer Scheme",
+                   Name = "Authorization",
+                   In = ParameterLocation.Header,
+                   Type = SecuritySchemeType.Http,
+                   Scheme = "bearer",
+                   Reference = new OpenApiReference
+                   {
+                       Type = ReferenceType.SecurityScheme,
+                       Id = "Bearer"
+                   }
+               };
+
+               c.AddSecurityDefinition("Bearer", securitySchema);
+
+               var securityRequirements = new OpenApiSecurityRequirement{
+                {securitySchema, new[] {
+                    "Bearer"
+                }}
+               };
+
+               c.AddSecurityRequirement(securityRequirements);
+
+           });
+
             return services;
         }
 
-        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app){
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
 
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
